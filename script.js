8a18b4955afa4db1a573ae0e18e09aa2713c6e05
sachin <sachinrajputlive@gmail.com>
2022-01-28 23:37:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const div1 = document.getElementById("array1").innerHTML = "array 1"
+const div2 = document.getElementById("array2").innerHTML = "array 2"
+let resultDiv = document.getElementById("result")
+resultDiv.innerHTML = 'result'
+
+const arrayOne = [
+    { value: "1", display: "a" },
+    { value: "2", display: "b" }
+];
+
+const arrayTwo = [
+    { value: "12", display: "a" },
+    { value: "2", display: "b" }
+];
+
+const results = arrayOne.filter(({ value: id1 }) => arrayTwo.some(({ value: id2 }) => id2 === id1));
+resultDiv.innerHTML = results[0].value;
+console.log(results);
